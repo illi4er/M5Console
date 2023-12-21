68d2a6625ae7ee9486e4a5026e51f7180e3bea6e
[IllI] <93869928+illi4er@users.noreply.github.com>
2023-12-21 18:28:45 +0500

    Create example.py

---
+++ example.py
@@ -0,0 +1,13 @@
+from m5stack import *
+from m5stack_ui import *
+from uiflow import *
+import console
+
+console.clear("Testing Console")
+
+console.write("Test")
+
+console.write("TEST 2!", 1)
+console.write("test3!", 2)
+console.write("tEsT.", 3)
+
